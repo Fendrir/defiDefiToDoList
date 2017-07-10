132bdf6d5a39e810adc6b9d4f3656942ba14e997
Julien <papao1@hotmail.fr>
2017-07-10 12:12:21 +0200

    début du javascript ajout de la balise <p></p> par le jquery

---
+++ javascript/javascript.js
@@ -0,0 +1,13 @@
+alert("Bienvenue Jeune Padawan");
+
+// ajout de la balise <p>
+
+
+$("body").append("<p></p>");
+
+
+// classe filtre = filtre
+// le data = data-action => done => todo => all
+//  ajouter une ligne class = add
+// tache a ajouter = tacheAjout
+
