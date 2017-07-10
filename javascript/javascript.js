b35e933c27e6ee1bf922c09affa8d5f4c7febc16
Julien <papao1@hotmail.fr>
2017-07-10 12:28:02 +0200

    ajout de la liste en jquery

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -5,9 +5,11 @@ alert("Bienvenue Jeune Padawan");
 
 $("body").append("<p></p>");
 
+$("ul").append("<li></li>");
 
 // classe filtre = filtre
 // le data = data-action => done => todo => all
 //  ajouter une ligne class = add
 // tache a ajouter = tacheAjout
+// classe de liste = liste
 
